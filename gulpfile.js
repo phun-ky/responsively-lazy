d6c009a039653b5eca3d917023b340dca4c8c8be
Alexander Vassbotn Røyne-Helgesen <alexander@phun-ky.net>
2017-03-02 13:49:45 +0100

    Add gulp for builds

---
+++ gulpfile.js
@@ -0,0 +1,77 @@
+const gulp = require('gulp');
+const stylus = require('gulp-stylus');
+const rename = require("gulp-rename");
+const stylint = require('gulp-stylint');
+const sourcemaps = require('gulp-sourcemaps');
+const cssnano = require('gulp-cssnano');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const pump = require('pump');
+
+gulp.task('compress', function (cb) {
+  pump([
+        gulp.src('src/responsivelyLazy.bundle.js'),
+        uglify(),
+        .pipe(rename('responsivelyLazy.min.js'))
+        gulp.dest('./src')
+    ],
+    cb
+  );
+});
+
+gulp.task('build:js', () => {
+    return gulp.src('src/responsivelyLazy.js')
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(rename('responsivelyLazy.bundle.js'))
+        .pipe(gulp.dest('./src'));
+});
+
+// Get one .styl file and render
+gulp.task('build:css:prod', function () {
+  return gulp.src('./src/responsivelyLazy.styl')
+    .pipe(stylint())
+    .pipe(stylint.reporter())
+    .pipe(stylus({
+      'include css': true,
+      'disable cache': true
+    }))
+    .pipe(rename('responsivelyLazy.min.css'))
+    .pipe(sourcemaps.init())
+    .pipe(cssnano({
+      reduceIdents: false,
+      discardDuplicates: true,
+      discardComments: {
+        removeAll: true
+      },
+      autoprefixer: {
+        add: true,
+        browsers: ['> 1%', 'last 2 versions'],
+        cascade: false
+      },
+      zindex: false
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./'));
+});
+
+gulp.task('build:css:dev', function () {
+  return gulp.src('./src/responsivelyLazy.styl')
+    .pipe(stylus({
+      compress: false,
+      'include css': true,
+      'disable cache': true
+    }))
+    .pipe(rename('responsivelyLazy.css'))
+    .pipe(gulp.dest('./'));
+});
+
+// Get one .styl file and render
+gulp.task('lint', function () {
+  return gulp.src('./src/responsivelyLazy.styl')
+    .pipe(stylint())
+    .pipe(stylint.reporter());
+});
+
+gulp.task('default', ['lint', 'build:css:dev']);
